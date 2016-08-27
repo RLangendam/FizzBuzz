8022edff589e776006dfe1aed87fef8b3a2861c0
Ralph Langendam <rlangendam@gmail.com>
2016-08-28 00:20:35 +0200

    The FizzBuzz application meets the spec and runs testers afterwards.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
-#define CATCH_CONFIG_MAIN
+#include <iterator>
+#include <iostream>
+#include "FizzBuzz.hpp"
+
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+int main(int argc, char** argv) {
+	FizzBuzz(std::ostream_iterator<std::string>(std::cout, "\n"));
+	return Catch::Session().run(argc, argv);
+}
