b4c5830eeba6ee48c3b7d313aa90a57c63fcdf6f
Ralph Langendam <rlangendam@gmail.com>
2016-08-27 23:20:03 +0200

    FizzBuzz outputs the first 100 natural numbers.

--- FizzBuzz.hpp
+++ FizzBuzz.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <algorithm>
+#include <string>
 
 static std::size_t const outputCount = 100u;
 
 template <typename OutputIterator>
 void FizzBuzz(OutputIterator output) {
-	std::generate_n(output, outputCount, []() -> std::string {
-		return "1";
+	std::size_t counter(1u);
+	std::generate_n(output, outputCount, [&counter](){
+		return std::to_string(counter++);
 	});
 }
--- FizzBuzzTester.cpp
+++ FizzBuzzTester.cpp
@@ -8,4 +8,5 @@ SCENARIO("FizzBuzz") {
 	FizzBuzz(std::back_inserter(output));
 	REQUIRE(output.size() == outputCount);
 	REQUIRE(output.at(0) == "1");
+	REQUIRE(output.at(1) == "2");
 }
